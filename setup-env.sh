5ea05de6778b535fe109a4a7b2d2bfdfc4ac1a43
Eli Miller <elirnm@uw.edu>
2017-12-20 15:06:24 -0800

    Update pip as part of installation

--- setup-env.sh
+++ setup-env.sh
@@ -11,6 +11,7 @@ echo "Entering virtual environment"
 source "$BASE/env/bin/activate"
 
 echo "Fetching dependencies"
+pip install -U pip # make sure pip is fully up-to-date
 pip install docopt~=0.6 scipy~=0.19 scikit-learn~=0.19 requests~=2.18
 
 # Freki is not in PyPI, so install it from git if it isn't already installed
